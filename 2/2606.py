81ebfd1d007ac1b69e767ac0a3d7ced4afb67260
Yeonwoo JI <speciling@naver.com>
2023-07-02 21:39:51 +0900

    Update 2606.py

--- 2/2606.py
+++ 2/2606.py
@@ -1,20 +1,18 @@
-from sys import stdin
-
-n = int(stdin.readline())
-infected = [0] * (n+1)
+import sys
+input = sys.stdin.readline
+n = int(input())
+visited = [False]*(n+1)
 graph = [[] for _ in range(n+1)]
-for _ in range(int(stdin.readline())):
-    a, b = map(int, stdin.readline().split())
+for _ in range(int(input())):
+    a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-
-
-def dfs(graph, v, visited):
-    visited[v] = 1
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
-
-
-dfs(graph, 1, infected)
-print(infected.count(1)-1)
+stk = [1]
+cnt = 0
+while stk:
+    now = stk.pop()
+    if not visited[now]:
+        visited[now] = True
+        stk += graph[now]
+        cnt += 1
+print(cnt-1)
